3562639077fc3df9554359b6cf8a728b18cbee72
Tim Welch <tim.j.welch@gmail.com>
2018-11-26 17:31:09 -0800

    initial commit

---
+++ data/agency.js
@@ -0,0 +1,3 @@
+var agency = [
+{"agency_id":"cherriots-salem-or-us","agency_name":"Cherriots","agency_url":"http://www.cherriots.org","agency_timezone":"America/Los_Angeles","agency_lang":"en","agency_phone":"503-588-2877","agency_fare_url":"http://cherriots.org/en/fares"}
+]
